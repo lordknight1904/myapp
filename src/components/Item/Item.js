6c99ffa76d70e645d7e7bfea265fac49e081a084
Anh Khoa Ngô <anhkhoango@Anhs-MacBook-Pro.local>
2018-05-01 22:56:59 +0700

    1/5 5

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -35,7 +35,7 @@ class Item extends Component {
               gutterBottom
               variant="display1"
             >
-              Lizard
+              Clothes
             </Typography>
             <Typography
               align="justify"
